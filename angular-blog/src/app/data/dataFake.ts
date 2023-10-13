c336a21f029fed4f75c10fd1fcb1beac81a9aa54
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-13 09:52:20 -0300

    content create

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title":"saiu novo filme",
-    "description":"ola"
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":"Marvel Studio anuncia novo filme do Homem de Ferro",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
   },
   {
-    "id":2,
-    "photo":"aaa",
-    "title":"saiu novo filme",
-    "description":"ola"
+    "id":"2",
+    "title":"NOVA SÉRIE ANUNCIADA",
+    "description":"Novo serie será lançada em breve",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+  "id":"3",
+  "title":"NOVO FILME DO PANTERA",
+  "description":"Novo filme do pantera negra lançado em breve",
+  "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+},
+  {
+    "id":"4",
+    "title":"NOVA FILME ANUNCIADO",
+    "description":"Novo filme dos X-men está em pós produção, afirma diretor",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  photoCover:string ="https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
-  contentTitle:string ="MINHA NOTICIA"
-  contentDescription:string ="  Olá Mundo!"
+  photoCover:string =""
+  contentTitle:string =""
+  contentDescription:string =""
+  private id:string|null = "0"
 
-  constructor() { }
+  constructor(
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+   
   }
 
 }
