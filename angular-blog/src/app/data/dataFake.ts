bd3500c99a4f10673542bcc9d2ca128b6a2b5cfd
Marcio Tanan <marcio-tanan@hotmail.com>
2023-10-13 10:15:47 -0300

    end project

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -22,5 +22,53 @@ export const dataFake = [
     "title":"NOVA FILME ANUNCIADO",
     "description":"Novo filme dos X-men está em pós produção, afirma diretor",
     "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  },
+  {
+    "id":"5",
+    "title":"NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":"Marvel Studio anuncia novo filme do Homem de Ferro",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+  },
+  {
+    "id":"6",
+    "title":"NOVA SÉRIE ANUNCIADA",
+    "description":"Novo serie será lançada em breve",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+  "id":"7",
+  "title":"NOVO FILME DO PANTERA",
+  "description":"Novo filme do pantera negra lançado em breve",
+  "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+},
+  {
+    "id":"8",
+    "title":"NOVA FILME ANUNCIADO",
+    "description":"Novo filme dos X-men está em pós produção, afirma diretor",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
+  },
+  {
+    "id":"9",
+    "title":"NOVO HOMEM DE FERRO ANUNCIADO",
+    "description":"Marvel Studio anuncia novo filme do Homem de Ferro",
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+  },
+  {
+    "id":"10",
+    "title":"NOVA SÉRIE ANUNCIADA",
+    "description":"Novo serie será lançada em breve",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+  "id":"11",
+  "title":"NOVO FILME DO PANTERA",
+  "description":"Novo filme do pantera negra lançado em breve",
+  "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+},
+  {
+    "id":"12",
+    "title":"NOVA FILME ANUNCIADO",
+    "description":"Novo filme dos X-men está em pós produção, afirma diretor",
+    "photoCover":"https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
